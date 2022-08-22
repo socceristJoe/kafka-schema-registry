fa4a99f6f9ef0e58f5cb9489f0c56838aea3fe72
Qiao, Joe <qiao.y@pg.com>
2022-08-22 16:17:58 +0800

    add avro-tools jar

--- 1-avro-java/avro-tools.sh
+++ 1-avro-java/avro-tools.sh
@@ -2,8 +2,8 @@
 wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
 
 # run this from our project folder. Make sure ~/avro-tools-1.8.2.jar is your actual avro tools location
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-generic.avro 
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-specific.avro 
+java -jar avro-tools-1.8.2.jar tojson --pretty ../kafka-avro-course-master/customer-generic.avro
+java -jar avro-tools-1.8.2.jar tojson --pretty ../kafka-avro-course-master/customer-specific.avro 
 
 # getting the schema
 java -jar ~/avro-tools-1.8.2.jar getschema customer-specific.avro 
